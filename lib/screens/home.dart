1e3a1221aff51a51922887cf8d32ef8a65e42ada
rajesh2701 <rajesh.vemula91@gmail.com>
2021-10-01 10:56:40 +0530

    Our Work goes HERE

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -10,6 +10,30 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool _coffeePurchased = false;
+  bool _pizzaPurchased = false;
+  bool _coffeeMonthlyPurchased = false;
+  bool _pizzaMonthlyPurchased = false;
+
+  @override
+  void initState() {
+    super.initState();
+    //checking only coffee
+    productsCheck(AppEnvironment.buyMeCoffee);
+  }
+
+  // HERE WE CHECK PRODCUTS PURCHASEORNOT
+  Future<void> productsCheck(String productKey) async {
+    // the code goes here
+    setState(() {
+      // if user purchase the product
+      _coffeePurchased = false;
+      _pizzaPurchased = true;
+      _coffeeMonthlyPurchased = true;
+      _pizzaMonthlyPurchased = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,20 +61,26 @@ class _HomeState extends State<Home> {
         ),
         _divider(30),
         _purchaseIAPButton(
-            'Buy Me Coffee One time', AppEnvironment.buyMeCoffee),
+            'Coffee', _coffeePurchased, AppEnvironment.buyMeCoffee),
         _divider(15),
-        _purchaseIAPButton('Buy me Pizza One Time', AppEnvironment.buyMePizza),
+        _purchaseIAPButton('Pizza', _pizzaPurchased, AppEnvironment.buyMePizza),
         _divider(50),
         const Text(
           'Subscription',
           style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
         ),
         _divider(15),
-        _purchaseIAPButton(
-            'Buy Me Coffee Monthly', AppEnvironment.buyMeCoffeeMOnthly),
+        _coffeeMonthlyPurchased
+            ? _subscripeIAPButton(
+                'Buy Me Coffee Monthly', AppEnvironment.buyMeCoffeeMOnthly)
+            : _cancelIAPSubscription(
+                'Coffee', AppEnvironment.buyMeCoffeeMOnthly),
         _divider(15),
-        _purchaseIAPButton(
-            'Buy me Pizza Monthly', AppEnvironment.buyMePizzaMonthlyy),
+        _pizzaMonthlyPurchased
+            ? _subscripeIAPButton(
+                'Buy Me Pizza Monthly', AppEnvironment.buyMePizzaMonthlyy)
+            : _cancelIAPSubscription(
+                'Pizza', AppEnvironment.buyMePizzaMonthlyy),
         _divider(50),
         const Text('Restore Purchases',
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
@@ -60,12 +90,37 @@ class _HomeState extends State<Home> {
     );
   }
 
-  Widget _purchaseIAPButton(String buttonName, String productKey) {
+  Widget _purchaseIAPButton(
+      String buttonName, bool purchaseStatus, String productKey) {
     return ElevatedButton(
         onPressed: () {
           // here we subscripe or buy products
+          if (purchaseStatus) {
+            // button click wont do anything
+          } else {
+            // purchase flow goes here
+          }
+        },
+        // teXT NAEM CHANE ACCORIDING TO PURCHASE
+        child: Text(purchaseStatus
+            ? 'Thank You for $buttonName'
+            : 'Buy Me $buttonName One Time'));
+  }
+
+  Widget _subscripeIAPButton(String buttonName, String productKey) {
+    return ElevatedButton(
+        onPressed: () {
+          // here we subscripe
         },
         child: Text(buttonName));
+  }
+
+  Widget _cancelIAPSubscription(String title, String key) {
+    return ElevatedButton(
+        onPressed: () {
+          // cancel subscribe code goes here
+        },
+        child: Text('Cancel $title Subsription'));
   }
 
   Widget _restorePurchases(String text) {
